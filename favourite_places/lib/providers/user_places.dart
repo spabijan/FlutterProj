aa832cf6ecbf7206c17e7e5ee3724f9494188c1b
spabijan <pabi@toya.net.pl>
2024-12-31 15:27:13 +0100

    native function app done

--- favourite_places/lib/providers/user_places.dart
+++ favourite_places/lib/providers/user_places.dart
@@ -1,14 +1,64 @@
+import 'dart:ffi';
+import 'dart:io';
+
 import 'package:favourite_places/model/place.dart';
+import 'package:favourite_places/model/place_location.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 final userPlacesProvider =
     StateNotifierProvider<UserPlacesNotifier, List<Place>>(
         (ref) => UserPlacesNotifier());
 
+Future<Database> _getDatabase() async {
+  final dbpath = await sql.getDatabasesPath();
+  return await sql.openDatabase(path.join(dbpath, 'places.db'),
+      onCreate: (db, version) {
+    return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lon REAL, address TEXT)');
+  }, version: 1);
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(Place newPlace) {
-    state = [newPlace, ...state];
+  Future<void> loadPlaces() async {
+    var db = await _getDatabase();
+    var data = await db.query('user_places');
+    var places = data
+        .map((row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+                latitude: row['lat'] as double,
+                longitude: row['lon'] as double,
+                address: row['address'] as String)))
+        .toList();
+    state = places;
+  }
+
+  void addPlace(Place newPlace) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(newPlace.image.path);
+    final copiedImage = await newPlace.image.copy('${appDir.path}/$fileName');
+
+    var updatedPlace = Place(
+        title: newPlace.title, image: copiedImage, location: newPlace.location);
+
+    var db = await _getDatabase();
+
+    db.insert('user_places', {
+      'id': updatedPlace.id,
+      'title': updatedPlace.title,
+      'image': copiedImage.path,
+      'lat': updatedPlace.location.latitude,
+      'lon': updatedPlace.location.longitude,
+      'address': updatedPlace.location.address
+    });
+    state = [updatedPlace, ...state];
   }
 }
--- favourite_places/lib/screens/map.dart
+++ favourite_places/lib/screens/map.dart
@@ -35,11 +35,13 @@ class _MapScreenState extends State<MapScreen> {
         ],
       ),
       body: GoogleMap(
-          onTap: (position) {
-            setState(() {
-              _pickedLocation = position;
-            });
-          },
+          onTap: !widget.isSelecting
+              ? null
+              : (position) {
+                  setState(() {
+                    _pickedLocation = position;
+                  });
+                },
           markers: (_pickedLocation == null && widget.isSelecting)
               ? {}
               : {
